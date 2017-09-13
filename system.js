8b23be376eb7666480ec67f02758c6b76a60dc72
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-09-13 19:48:55 -0400

    Update system.js

--- system.js
+++ system.js
@@ -1,1 +1,18 @@
-
+'use strict';
+ function a(){
+    function log(str) {
+        console.log(str);
+        console.log(undefined);
+    }
+    var api = function check(api) {
+    var name = 'asap';
+    var i;
+    if (i === undefined || i === 0){
+            i = 1;
+                while (i < 1000 && i !== undefined) {
+                    i++;
+                    log(i);
+                }
+            }
+        };
+}
