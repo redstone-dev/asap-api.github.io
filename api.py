8dc10717698bbcfbc6a130924e26ed9495c47925
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-09-14 17:57:52 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,12 @@
+"""
 async = True
 print async
+"""
 big = True
 print str(big)
+print('loading java [70%]')
+print('done')
+queryString = "querystring=java?true+jsLoaded?true+api?=asap+id?=r4OjzcA"
+print queryString
+api = "asap"+queryString+"poweredByPython"+queryString
+print api
