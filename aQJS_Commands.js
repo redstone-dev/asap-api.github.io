2b522f5980c75779d25c91e01afcfa32dbdbed7c
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-09-14 16:58:57 -0400

    Create aQJS_Commands.js

---
+++ aQJS_Commands.js
@@ -0,0 +1,7 @@
+/*
+aQJS Commands:
+  grab[src=[file://filename-filename-extension.zip]] << Downloads stuff as '.zip' locally in the .aqjs file
+  abort[src=[file://filename-filename-extension.zip/inFile?aqjs-filename.aqjs]] << Does oppisite of grab
+  js{// some js;} << adds JS in the .aqjs file (Max: 100000000 lines)
+  html{<!--some html-->} << same as js{}, but html (Max: 100000000 lines)
+*/
