9c216ebf929ab8c8bd9ecb8a288fb02841b6a19e
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-09-14 17:02:21 -0400

    Update and rename aQJS_Commands.js to aQJS-Commands.js

--- aQJS-Commands.js
+++ aQJS-Commands.js
@@ -2,6 +2,7 @@
 aQJS Commands:
   grab[src=[file://filename-filename-extension.zip]] << Downloads stuff as '.zip' locally in the .aqjs file
   abort[src=[file://filename-filename-extension.zip/inFile?aqjs-filename.aqjs]] << Does oppisite of grab
-  js{// some js;} << adds JS in the .aqjs file (Max: 100000000 lines)
+  js{// some js} << adds JS in the .aqjs file (Max: 100000000 lines)
   html{<!--some html-->} << same as js{}, but html (Max: 100000000 lines)
+  loadData[src=[filetype%dataurl.filename-extension] type=[filetype]] << loads external data
 */
