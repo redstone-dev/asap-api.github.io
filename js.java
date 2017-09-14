99f98f664ff739210d52cb8adc674e89a9044b8e
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-09-14 17:50:03 -0400

    Update and rename jav.java to js.java

---
+++ js.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+  System.out.printIn("loading asap [10%=10prcnt]")
+  System.out.printIn("done loading asap [100%=100prcnt]")
+  System.out.printIn("warning='slow' | loading current version of java [1%=1prcnt]")
+	System.out.printIn("stage='all-done' ")
+	System.out.printIn("reboot='true' ")
+	System.out.printIn("warning='reboot' stage='reboot' | rebooting")
+	System.out
+	System.out.printIn("add='/wow/' queryString='api?=asap/type=java/js' ")
+};
