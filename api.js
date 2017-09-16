2bae3bd0369d9071141d76fde0f36c029d6689ae
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-09-16 15:10:22 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,16 @@
-
+var version = 1.0;
+var API = {
+    "async":true,
+    "JSModule":true,
+    "name":"asap",
+    "jQ":true,
+    "properties":{
+        "fast":true,
+        "jQ":true,
+        "Python":true,
+        "java":{
+            "supported":true
+        },
+        
+    }
+};
