6528d2c69da9cce6afaae0f1a5a28062d4fe82d7
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-09-16 08:30:18 -0400

    Update js.java

--- js.java
+++ js.java
@@ -5,6 +5,9 @@ public static void main(String[] args) {
 	System.out.printIn("stage='all-done' ")
 	System.out.printIn("reboot='true' ")
 	System.out.printIn("warning='reboot' stage='reboot' | rebooting")
-	System.out
-	System.out.printIn("add='/wow/' queryString='api?=asap/type=java/js' ")
+	
+		
+	System.out.printIn("add['/wow/'] queryString=['apiname?=asap&type=text-javascript&type=text-java&path?=libs/type=javascript/asap.js/']")
+	System.out.printIn("add[queryString]")
+	System.out.printIn("addToURL(java?=enabled&javascript?=enabled&jq?=disableORenable)")
 };
